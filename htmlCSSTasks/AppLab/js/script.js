9b7fc7558d82cdaa22b5a6b1bebc18a847a270c9
Jashpatel7 <77331643+Jashpatel7@users.noreply.github.com>
2021-02-15 14:52:38 +0530

    Add files via upload

---
+++ htmlCSSTasks/AppLab/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+            $('[data-toggle="collapse"]').click(function() {
+                $(this).toggleClass("active");
+                if ($(this).hasClass("active")) {
+                    $(this).text("Hide");
+                } else {
+                    $(this).text("Show");
+                }
+            });
